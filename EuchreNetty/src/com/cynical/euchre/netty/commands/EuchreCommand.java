abbbf5973887caea4b00d3b576772c92ac1af3f5
abenjamin <abenjamin@gmail.com>
2012-04-08 22:38:04 -0500

    Adding a String value to command enum

--- EuchreNetty/src/com/cynical/euchre/netty/commands/EuchreCommand.java
+++ EuchreNetty/src/com/cynical/euchre/netty/commands/EuchreCommand.java
@@ -5,22 +5,32 @@ public enum EuchreCommand {
 	//////////////////////////////
 	//	Pre-Game Commands
 	//////////////////////////////
-	LOGIN,
-	REQUEST_LOBBY_REFRESH,
-	JOIN_GAME,
+	LOGIN("LOGIN"),
+	REQUEST_LOBBY_REFRESH("REQUEST_LOBBY_REFRESH"),
+	JOIN_GAME("JOIN_GAME"),
 	
 	//////////////////////////////
 	//	In-Game Commands
 	//////////////////////////////
-	REQUEST_GAME_STATE,
-	ORDER_UP,
-	PASS,
-	DISCARD,
-	PLAY_CARD,
+	REQUEST_GAME_STATE("REQUEST_GAME_STATE"),
+	ORDER_UP("ORDER_UP"),
+	PASS("PASS"),
+	DISCARD("DISCARD"),
+	PLAY_CARD("PLAY_CARD"),
 	
 	//////////////////////////////
 	//	Error
 	//////////////////////////////
-	NOT_A_COMMAND;
+	NOT_A_COMMAND("NOT_A_COMMAND");
+	
+	private String value;
+	
+	private EuchreCommand(String s) {
+		this.value = s;
+	}
+	
+	public String toString() {
+		return this.value;
+	}
 
 }
